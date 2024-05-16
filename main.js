08c6a34ddee1dfab5c5d39b7fa5091f273b8d1ea
mariagabriela123gabi <110062972+mariagabriela123gabi@users.noreply.github.com>
2024-05-16 08:09:47 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+const botoes = document.querySelectorAll(".botao")
+console.log(botoes)
+for (let i = 0; i < botoes.length ; i++){
+   botoes[i].onclick = function() {
+      for (j=0;<botoes.length;j++){
+         botoes[j].classList.remove("ativo");
+      
+      }
+      botoes[i].classList.add("ativo")
+   }
+}
