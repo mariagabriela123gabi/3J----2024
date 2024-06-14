2556e41fc96f880deed06f6cbd4e21dc917b9bae
mariagabriela123gabi <110062972+mariagabriela123gabi@users.noreply.github.com>
2024-06-14 09:42:21 -0300

    14/07

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 const botoes = document.querySelectorAll(".botao");
 const conteudo = document.querySelectorAll(".aba-conteudo");
 const tempo = document.querySelectorAll(".contador");
+ tempo[0].textContent = "ola"
+ const tempoObjetivo1 = new Date(14);
+ const tempoObjetivo2 = new Date(05);
+ const tempoObjetivo3 = new Date(2007);
+ const tempoObjetivo4 = new Date(07);
+ const tempoObjetivo5 = new Date(30);
+ const agora = new Date();
+ tempo[1].textContent = agora
 //console.log(botoes);
 for(let i = 0; 1 < botoes.length ; i++){
     botoes[i].onclick = function() {
