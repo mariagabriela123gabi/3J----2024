4964109937a1becee795cf6bef8003450da92935
mariagabriela123gabi <110062972+mariagabriela123gabi@users.noreply.github.com>
2024-07-04 08:09:02 -0300

    04/07

--- main.js
+++ main.js
@@ -2,11 +2,11 @@ const botoes = document.querySelectorAll(".botao");
 const conteudo = document.querySelectorAll(".aba-conteudo")
 const tempo = document.querySelectorAll(".contador")
 tempo[0].textContent = "hello,tudo bem?"
-const tempoObjetivo1 = new Date(2024,14,20,05);
-const tempoObjetivo2 = new Date(016);
+const tempoObjetivo1 = new Date(2024,07,04,7);
+const tempoObjetivo2 = new Date(16);
 const tempoObjetivo3 = new Date(2007);
 const tempoObjetivo4 = new Date(17);
-const tempoObjetivo5 = new Date(06);
+const tempoObjetivo5 = new Date(05);
 const agora = new Date();
 tempo[0].textContent = tempoObjetivo1-agora
 tempo[1].textContent = tempoObjetivo1
@@ -22,6 +22,10 @@ segundos = Math.floor(segundos);
 minutos = Math.floor(minutos);
 horas = Math.floor(horas);
 dias = Math.floor(dias);
+
+segundos = segundos%60
+minutos = minutos%60
+horas = horas%24
 tempo[0].textContent = `faltam ${dias} dias,${horas} horas,${minutos} minutos, ${segundos} segundos`
 segundos=Math.floor(segundos);
 console.log(botoes);
